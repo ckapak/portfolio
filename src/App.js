ed04e93423fe04a41c840a48ced1e01ddd3658b6
ckapak <christinekapak@googlemail.com>
2020-03-10 10:43:42 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './App.css'
+import Typical from 'react-typical'
 
-function App() {
-  return (
-    <h1>Hello werl</h1>
-  )
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello! My name is Christine Kapak</h1>
+          <p>I am a{' '}
+            <Typical
+            loop={Infinity}
+            wrapper="p"
+            steps={[
+              'front end developer', 1000, 
+              'marketing person', 1000,
+              'ski enthuasist', 1000
+              ]}
+            />
+          </p>
+        </header>
+      </div>
+
+    )
+  }
 }
 
 export default App
