786fc9db6075602a9677c2187b0e6c5dfaefcc10
ckapak <christinekapak@googlemail.com>
2020-03-14 21:08:51 +0000

    upgraded acorn

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './stylesheets/main.scss'
 import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
-import Navbar from './components/Navbar'
 import Contact from './components/Contact'
 import Skills from './components/Skills'
 
 const App = () => {
   return (
     <main>
-      <Navbar />
       <Home />
       <About />
       <Skills />
